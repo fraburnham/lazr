0c7226690c3f13f2701ec76c7cf3b41e35621011
Frank Burnham <frank@n8git.dev>
2022-11-29 16:53:03 -0500

    Add some brightness correction to help photos show on wood

--- src/lazr/greyscale/transform.clj
+++ src/lazr/greyscale/transform.clj
@@ -4,7 +4,8 @@
            java.awt.image.BufferedImage
            java.awt.image.IndexColorModel))
 
-(def type->key
+;; https://docs.oracle.com/javase/8/docs/api/java/awt/image/BufferedImage.html
+(def image-type->key
   {BufferedImage/TYPE_3BYTE_BGR :3-byte-bgr
    BufferedImage/TYPE_4BYTE_ABGR :4-byte-abgr
    BufferedImage/TYPE_4BYTE_ABGR_PRE :4-byte-abgr-pre
@@ -54,27 +55,49 @@
 
 (def ->luminosity (partial ->weighted {:r 0.299 :g 0.587 :b 0.114}))
 
+(defn- indexed-greys
+  "Shifts the number of white bias greys defined by the in ratio up to the out ratio portion of the output [0, max)
+  (indexed-greys 5 3 1/2 10) ; I want 5 greys. Give the top 3 of them a color that is in the top 1/2 of [0, 10)
+    => (0 2 5 6 8)"
+  ([num-greys in out]
+   (indexed-greys num-greys in out 256))
+  ([num-greys in out max]
+   (let [black-step-size (/ (- max (- max (* out max))) (- num-greys in))
+         white-step-size (/ (- max (* out max)) in)]
+     (byte-array
+      (map
+       int
+       (concat
+        (map #(+ % (/ black-step-size 2))
+             (range 0 (- max (* out max) 1) black-step-size))
+        (map #(+ % (/ white-step-size 2))
+             (range (- max (* out max)) max white-step-size))))))))
+
+;; TODO: long term I wonder if I could use a histogram to determine how to shift the values automagically...
+;; TODO: spec for squash?
 (defn ->indexed
-  [image num-greys]
-  (let [type (type->key (.getType image))]
-    (when-not (or (= type :ushort-gray)
-                  (= type :byte-gray))
-      (throw (ex-info "Cannot convert to indexed grey image. Must be :ushort-gray or :byte-gray" {:type type}))))
-  (let [step (/ 256 num-greys)
-        values (byte-array (map #(math/floor (+ % (/ step 2))) (range 0 255 step))) ; values are in the middle of their step
-        color-model (IndexColorModel. 8 num-greys values values values)
-        new-image (BufferedImage. (.getWidth image)
-                                  (.getHeight image)
-                                  BufferedImage/TYPE_BYTE_INDEXED
-                                  color-model)
-        rast (.getData image)
-        data (.getDataBuffer rast)
-        scale (make-scaler [0 255] 0 [0 (bit-shift-left 1 (.getPixelSize (.getColorModel image)))])]
-    (dotimes [i (.getSize data)]
-      (let [value (->> (.getElem data i)
-                       (scale)
-                       (#(/ % step))
-                       (math/floor))]
-        (.setElem data i value)))
-    (.setData new-image rast)
-    new-image))
+  ([image num-greys]
+   (->indexed image num-greys {:in 1 :out (- 1 (/ 1 num-greys))}))
+  ([image num-greys {:keys [in out]}]
+   (let [type (image-type->key (.getType image))]
+     (when-not (or (= type :ushort-gray)
+                   (= type :byte-gray))
+       (throw (ex-info "Cannot convert to indexed grey image. Must be :ushort-gray or :byte-gray" {:type type}))))
+   (let [step (/ 256 num-greys)
+         values (indexed-greys num-greys in out)
+         color-model (IndexColorModel. 8 num-greys values values values)
+         new-image (BufferedImage. (.getWidth image)
+                                   (.getHeight image)
+                                   BufferedImage/TYPE_BYTE_INDEXED
+                                   color-model)
+         rast (.getData image)
+         data (.getDataBuffer rast)
+         scale (make-scaler [0 255] 0 [0 (bit-shift-left 1 (.getPixelSize (.getColorModel image)))])]
+     (dotimes [i (.getSize data)]
+       (let [value (->> (.getElem data i)
+                        (scale)
+                        (#(/ % step))
+                        (math/floor))]
+         (.setElem data i value)))
+     (.setData new-image rast)
+     new-image)))
