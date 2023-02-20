8840a00a936087ac464bffb47443ce35357470b0
Frank Burnham <frank@n8git.dev>
2023-02-20 08:00:20 -0500

    Pretty sure this is all dead code.

--- src/lazr/file.clj
+++
@@ -1,3 +0,0 @@
-(ns lazr.file)
-
-;; This ns should handle converting a file to a dict that has the "type" (which will usually be extension, but _maybe_ will be extended to handle the diff greys or w/e) 
